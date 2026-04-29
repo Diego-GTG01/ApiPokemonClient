e58b9378a4c459e6a1cecbda8f603bb40a782e0c
Diego GTG <diegogomeztg@outlook.com>
2026-04-29 14:26:51 -0600

    Ficha entrenador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { VistaMain } from './Component/vista-main/vista-main';
+import { GestionUsuarios } from './Component/gestion-usuarios/gestion-usuarios';
 
 export const routes: Routes = [
-    {
-        path: "", component: VistaMain
-    }
+  {
+    path: '',
+    component: VistaMain,
+  },
+  {
+    path: 'PokeUsers',
+    component: GestionUsuarios,
+  },
 ];
